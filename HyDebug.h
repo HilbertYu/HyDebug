99e6646093b38e3363e969dc466eb89a4498436b
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2014-08-17 15:29:47 +0800

    +err_quit

--- HyDebug.h
+++ HyDebug.h
@@ -2,6 +2,7 @@
 #define HYDEBUG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
 #include <errno.h>
@@ -9,13 +10,20 @@
 #define HYDEBUG_MAXLINE 256
 
 
-static void err_doit(int, int, const char* fmt, va_list ap); 
+static void err_doit(int, int, const char* fmt, va_list ap);
 static void err_msg(const char* fmt, ...);
 
 #define HYDEBUG(fmt, args...) {\
     char buf[HYDEBUG_MAXLINE];\
     snprintf(buf, sizeof(buf), "dbg[%s:%d]%s", __FILE__, __LINE__, fmt);\
     err_msg(buf, ##args);\
+}
+
+#define HYDEBUG_QUIT(fmt, args...) {\
+    char buf[HYDEBUG_MAXLINE];\
+    snprintf(buf, sizeof(buf), "dbg[%s:%d]%s", __FILE__, __LINE__, fmt);\
+    err_msg(buf, ##args);\
+    exit(1);\
 }
 
 
@@ -29,9 +37,9 @@ void err_msg(const char* fmt, ...) {
 }
 
 
-static 
+static
 void err_doit(int errno_flag, int error, const char* fmt, va_list ap) {
-    
+
     char buf[HYDEBUG_MAXLINE];
     bzero(buf, sizeof(buf));
     vsnprintf(buf, HYDEBUG_MAXLINE, fmt, ap);
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main(int argc, const char *argv[])
 {
     int a = 10;
-    HYDEBUG("xx %d", a);
+    HYDEBUG_QUIT("xx %d", a);
  //   int a = 10;
 //    HyDebug("xx %d", a);
     return 0;
